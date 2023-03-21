f1cee26f746ed1f18512ffb2f894b7318884b8de
deva-ghb <deva.macha@gytworkz.com>
2023-03-21 15:33:07 +0530

    exception handling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import openai
-from flask import Flask, request
+from flask import Flask, request, Response
 from gpt import gptUtil
 
 app = Flask(__name__, instance_relative_config=True)
@@ -9,11 +9,20 @@ def ask():
     form_specification = request.json.get("specification")
     fields = request.json.get("fields")
     if isinstance(form_specification, str) and isinstance(fields, list):
-        form_json = gptUtil.formSpecificationToJson(form_specification, fields)
-        response = {
-            'form_json' : form_json
-        }
-    return response
+        try:
+            form_json = gptUtil.formSpecificationToJson(form_specification, fields)
+            response = {
+                'form_json' : form_json
+            }
+            return response
+        except Exception as e:
+            msg = str({'message': 'internal server error'})
+            return Response(msg, status=500, mimetype='application/json')
+            
+    else:
+        msg = str({'message': 'invalid input'})
+        return Response(msg, status=400, mimetype='application/json')
+        
 
 if __name__ == "__main__":
     app.run(port=5005, debug= True)
